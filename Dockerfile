0a3accfb547e2d7c55a132bb89e832489f697595
codermvn <dvarshney86@gmail.com>
2021-05-05 11:44:01 +0530

    changing for newRelic

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM openjdk:11
 WORKDIR /
+RUN mkdir -p /usr/local/tomcat/newrelic
+RUN mkdir -p /usr/local/tomcat/newrelic/logs
+#RUN chown -R tomcat:tomcat /usr/local/tomcat/newrelic/logs
+ADD newrelic-java/newrelic/newrelic.jar /usr/local/tomcat/newrelic/newrelic.jar
+ADD newrelic-java/newrelic/newrelic.yml /usr/local/tomcat/newrelic/newrelic.yml
 ADD target/test-0.0.1-SNAPSHOT.jar test.jar
+ADD start.sh .
 EXPOSE 8080
-CMD java -jar test.jar --spring.config.location=/test/application.properties
+ENV JAVA_OPTS="$JAVA_OPTS -javaagent:/usr/local/tomcat/newrelic/newrelic.jar"
+#CMD java -jar test.jar --spring.config.location=/test/application.properties
+#CMD java -Dnewrelic.environment=$ENV -jar test.jar --spring.config.location=/test/application.properties
+# start on run
+CMD ["bash", "start.sh"]
+#java -jar test.jar --spring.config.location=/test/application.properties
--- src/main/java/com/deepak/test/GreetingController.java
+++ src/main/java/com/deepak/test/GreetingController.java
@@ -1,7 +1,11 @@
 package com.deepak.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import com.deepak.test.exception.TFSBaseException;
+import com.deepak.test.service.CheckService;
 
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -12,6 +16,9 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
+	@Autowired
+	CheckService service;
+	
 	@Value("${app.version}")
     private String version;
     private static final String template = "Hello, %s!";
@@ -25,5 +32,10 @@ public class GreetingController {
     @RequestMapping(method = RequestMethod.GET, path="/test")
     public String getResponse(@RequestParam(value="input1",defaultValue = "Deepak") String name){
         return "Hi"+name+"appVersion:"+version;
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, path="/check")
+    public String getExceptioCheck(@RequestParam(value="input1") String input) throws TFSBaseException{
+        return service.Howdy(input);
     }
 }
